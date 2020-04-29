39b2743a556815082febad8b7d6d3ee29588f769
Tony Edwards <tony.edwards@gmail.com>
2020-04-29 18:38:20 +0100

    Adds habit tasks

--- functions/habits.js
+++ functions/habits.js
@@ -2,7 +2,7 @@
 
 const todoist = require('./todoist.js');
 const dateHelper = require('./helpers/dates.js');
-const responses = require('./responses/dailyTasks.js');
+const responses = require('./responses/habits.js');
 
 class HabitsController {
     constructor() {
@@ -15,19 +15,103 @@ class HabitsController {
     }
 
     async somethingStoic(){
+        console.info('Consume something stoic');
 
+        return new Promise(async (res, rej) => {
+
+            try {
+                const stoicTask = await todoist.getTask(this.stocicID);
+
+                if (dateHelper.isToday(new Date(stoicTask.due.date))) {
+                    console.info('Something stoic yet to be consumed');
+    
+                    await todoist.setTaskAsComplete(this.stocicID).then(() => {
+                        res(responses.stoic.success);
+                    });
+                } else {
+                    res(responses.stoic.notNeeded);
+                }
+            } catch(error){
+                rej(responses.stoic.failure + ': ' + error).then( () => {
+                    return responses.stoic.failure;
+                });
+            }
+        });
     }
 
     async learnGerman(){
+        console.info('Learning something German');
 
+        return new Promise(async (res, rej) => {
+
+            try {
+                const germanTask = await todoist.getTask(this.germanID);
+
+                if (dateHelper.isToday(new Date(germanTask.due.date))) {
+                    console.info(`You've not learned anything german yet`);
+    
+                    await todoist.setTaskAsComplete(this.germanID).then(() => {
+                        res(responses.learnGerman.success);
+                    });
+                } else {
+                    res(responses.learnGerman.notNeeded);
+                }
+            } catch(error){
+                rej(responses.learnGerman.failure + ': ' + error).then( () => {
+                    return responses.learnGerman.failure;
+                });
+            }
+        });
     }
 
     async clothesReady(){
+        console.info('Getting clothes ready for tomorrow');
 
+        return new Promise(async (res, rej) => {
+
+            try {
+                const clothesTask = await todoist.getTask(this.clothesID);
+
+                if (dateHelper.isToday(new Date(clothesTask.due.date))) {
+                    console.info(`You've not got your clothes ready yet`);
+    
+                    await todoist.setTaskAsComplete(this.clothesID).then(() => {
+                        res(responses.clothesReady.success);
+                    });
+                } else {
+                    res(responses.clothesReady.notNeeded);
+                }
+            } catch(error){
+                rej(responses.clothesReady.failure + ': ' + error).then( () => {
+                    return responses.clothesReady.failure;
+                });
+            }
+        });
     }
 
     async onePageOfABook(){
+        console.info('Reading one page of a book');
 
+        return new Promise(async (res, rej) => {
+
+            try {
+                const bookTask = await todoist.getTask(this.bookID);
+
+                if (dateHelper.isToday(new Date(bookTask.due.date))) {
+                    console.info(`You've not read a page of a book yeat`);
+    
+                    await todoist.setTaskAsComplete(this.bookID).then(() => {
+                        res(responses.onePage.success);
+                    });
+                } else {
+                    res(responses.onePage.notNeeded);
+                }
+            } catch(error){
+                rej(responses.onePage.failure + ': ' + error).then( () => {
+                    return responses.onePage.failure;
+                });
+            }
+        });
     }
 
 }
--- functions/responses/habits.js
+++ functions/responses/habits.js
@@ -1,15 +1,23 @@
 export const stoic =  {
-
+    success: 'I\'ve recorded that you\'ve consumed something stoic today.',
+    notNeeded: 'You don\'t need to be any more stoical today.',
+    failure: 'ERROR consuming something stoic'
 }
 
 export const learnGerman =  {
-
+    success: 'Well done for learning a new language.',
+    notNeeded: 'You don\'t need to do any more German today.',
+    failure: 'ERROR learning german'
 }
 
 export const clothesReady =  {
-
+    success: 'I\'ve noted that your clothes are ready.',
+    notNeeded: 'Your clothes are already out!',
+    failure: 'ERROR getting clothes ready'
 }
 
 export const onePage =  {
-
+    success: 'I\'ve noted that you\'ve read something.',
+    notNeeded: 'It looks like you\'ve already read something today.',
+    failure: 'ERROR reading one page of a book'
 }
