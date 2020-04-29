9c23323cfd11a5be4b90ad43a8d1a7759d5d7ab1
Tony Edwards <tony.edwards@gmail.com>
2020-04-29 18:17:30 +0100

    Converts responses to es6 module

--- functions/responses/dailyTasks.js
+++ functions/responses/dailyTasks.js
@@ -1,4 +1,4 @@
-exports.feedDexter =  {
+export const feedDexter =  {
     successMorning: 'I\'ve noted that Dexter has been fed this morning.',
     successEvening: 'I\'ve noted that Dexter has been fed this evening.',
     notNeeded: 'Dexter does not need to be fed right now.',
@@ -7,43 +7,43 @@ exports.feedDexter =  {
     failurePM: 'Error getting pm feed'
 };
 
-exports.doDishes = {
+export const doDishes = {
     success: 'I\'ve noted that the dishes have been done.',
     notNeeded: 'The dishes do not need doing right now.',
     failure: 'ERROR doing dishes'
 };
 
-exports.emptyLitterTray = {
+export const emptyLitterTray = {
     success: 'I\'ve noted that the litter tray has been emptied.',
     notNeeded: 'The litter tray does not need emptying right now.',
     failure: 'ERROR emptying litter tray'
 };
 
-exports.feedBirds = {
+export const feedBirds = {
     success: 'I\'ve noted that the birds have been fed.',
     notNeeded: 'The birds do not need feeding right now.',
     failure: 'ERROR feeding birds'
 };
 
-exports.rubbish = {
+export const rubbish = {
     success: 'I\'ve noted that the rubbish has been taken out.',
     notNeeded: 'The rubbish does not need taking out right now.',
     failure: 'ERROR taking out rubbish'
 };
 
-exports.recycling = {
+export const recycling = {
     success: 'I\'ve noted that the recycling has been taken out.',
     notNeeded: 'The recycling does not need taking out right now.',
     failure: 'ERROR taking out recycling'
 };
 
-exports.makeBed= {
+export const makeBed= {
     success: 'I\'ve noted that the bed has been made.',
     notNeeded: 'The bed dosen\'t need making right now.',
     failure: 'ERROR making the bed'
 };
 
-exports.waterHousePlants = {
+export const waterHousePlants = {
     success: 'I\'ve noted that the plants have been watered.',
     notNeeded: 'The plants do not need watering right now.',
     failure: 'ERROR watering plants'
